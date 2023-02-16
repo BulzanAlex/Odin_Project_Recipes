7a686e7f883c238c69591344c2997667bdc14a40
Bulzan Alex <bulzandavidalex@gmail.com>
2023-02-16 18:39:04 +0200

    added styling to popup

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function showPopUp(){
+    document.getElementsByName('first-name')[0].value = '';
+    document.getElementsByName('last-name')[0].value = '';
+    document.getElementsByName('email')[0].value = '';
+}
